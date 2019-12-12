4e77b6c38841014bdbe5d93e0b5997f0c3af0081
xianqing.zhang <iczdg6ldd5j5>
2019-12-12 17:43:09 +0800

    启动类增加日志输出

--- src/main/java/com/zxq/cloud/admin/AdminApplication.java
+++ src/main/java/com/zxq/cloud/admin/AdminApplication.java
@@ -1,5 +1,6 @@
 package com.zxq.cloud.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author zxq
  */
 @SpringBootApplication
+@Slf4j
 public class AdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+        log.info("============================server start==========================");
     }
 
 }
