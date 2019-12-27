36454bcbc7bf72fbe432dce1653b720b7aef901d
xianqing.zhang <iczdg6ldd5j5>
2019-12-27 10:18:32 +0800

    测试回滚已提交代码

--- src/test/java/com/zxq/cloud/admin/AdminTest.java
+++ src/test/java/com/zxq/cloud/admin/AdminTest.java
@@ -1,10 +1,13 @@
 package com.zxq.cloud.admin;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.IdcardUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 /**
  * @author zxq
@@ -15,10 +18,17 @@ public class AdminTest {
 
     @Test
     public void testHutoolApi(){
-        String idCard = "362322199512214811";
+        /*String idCard = "362322199512214811";
         String provinceByIdCard = IdcardUtil.getProvinceByIdCard(idCard);
         log.info("provinceByIdCard:{}",provinceByIdCard);
         int genderByIdCard = IdcardUtil.getGenderByIdCard(idCard);
-        log.info("genderByIdCard:{}",genderByIdCard);
+        log.info("genderByIdCard:{}",genderByIdCard);*/
+
+        String endPutTime = "2019-12-19 18:59:59";
+
+        int i = DateUtil.beginOfDay(new Date()).compareTo(DateUtil.parseDateTime(endPutTime));
+
+        System.out.println(i);
+
     }
 }
