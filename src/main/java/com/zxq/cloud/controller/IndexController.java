af7a541c31f27e9c6efd25886363b9421cd6a578
LinDaiDai <244508795@qq.com>
2020-02-13 17:49:21 +0800

    Revert "新增首页"
    This reverts commit f4cd61bb

--- src/main/java/com/zxq/cloud/controller/IndexController.java
+++ src/main/java/com/zxq/cloud/controller/IndexController.java
@@ -2,9 +2,7 @@ package com.zxq.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.zxq.cloud.service.RedisService;
 
@@ -13,20 +11,15 @@ import com.zxq.cloud.service.RedisService;
  * @date 2019/12/27 17:34
  **/
 @Slf4j
-@Controller
+@RestController
+@RequestMapping("/index")
 public class IndexController {
 
     @Autowired
     private RedisService redisService;
 
-    @ResponseBody
     @RequestMapping("get")
     public Object get() {
         return redisService.get("user");
-    }
-
-    @RequestMapping("index")
-    public String index() {
-        return "index";
     }
 }
