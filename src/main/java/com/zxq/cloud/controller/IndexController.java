f4cd61bb4aceba1441da62cfe75f12cdca4bb184
LinDaiDai <244508795@qq.com>
2020-02-13 17:47:03 +0800

    新增首页

--- src/main/java/com/zxq/cloud/controller/IndexController.java
+++ src/main/java/com/zxq/cloud/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.zxq.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.zxq.cloud.service.RedisService;
 
@@ -11,15 +13,20 @@ import com.zxq.cloud.service.RedisService;
  * @date 2019/12/27 17:34
  **/
 @Slf4j
-@RestController
-@RequestMapping("/index")
+@Controller
 public class IndexController {
 
     @Autowired
     private RedisService redisService;
 
+    @ResponseBody
     @RequestMapping("get")
     public Object get() {
         return redisService.get("user");
+    }
+
+    @RequestMapping("index")
+    public String index() {
+        return "index";
     }
 }
